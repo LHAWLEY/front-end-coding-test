6c58d72a77f50888bcf9ecf6ba52fe2f6b6d9e7a
LHAWLEY <hawleyleeanne@gmail.com>
2017-09-21 09:28:08 -0400

    final solution

--- src/example1/example1.js
+++ src/example1/example1.js
@@ -4,10 +4,19 @@ rh.test.flip = {
         trigger: "data-test-flip-on",
         visible: "data-test-visible"
     },
-    action: function( $el ) {}
+    action: function( $el ) {
+        const index = $( "[" + this.attr.trigger + "]" ).index( $el );
+        const direction = index % 2 === 0 ? "next" : "prev";
+
+        $el.toggle();
+        $el[ direction ]().toggle();
+    }
 };
 
 // For all flip-enabled elements, attach an on click or on hover event
 $( "[" + rh.test.flip.attr.trigger + "]", context ).each( function( idx, val ) {
     // Add your trigger event here
+    $( val ).on( "click", function() {
+        rh.test.flip.action( $( this ) );
+    } );
 } );
